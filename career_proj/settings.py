5f2b3e5340f36295f90676e9fea679f9983c50c6
Michael Safdieh <mikesafdieh@gmail.com>
2018-11-20 19:19:33 -0500

    add djago-rest-framework; update startproj notes; update requirements

--- career_proj/settings.py
+++ career_proj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
--- startproj.sh
+++ startproj.sh
@@ -1,27 +1,37 @@
-# setup project and virtual env
+### setup project and virtual env
 cd ~
 mkvirtualenv jobapps -p python3
 pip install Django
 django-admin startproject career_proj
 cd career_proj
 ./manage.py startapp jobapps
-# add ```'jobapps.apps.JobappsConfig',``` to INSTALLED_APPS in settings.py
-# add ```path('jobapps/', include('jobapps.urls')),``` to urlpatterns in career_proj/urls.py
+# add `'jobapps.apps.JobappsConfig',` to INSTALLED_APPS in settings.py
+# add `path('jobapps/', include('jobapps.urls')),` to urlpatterns in career_proj/urls.py
 
 pip install django-extensions
-# now add ```'django_extensions',``` to INSTALLED_APPS in settings.py
+pip install djangorestframework
+# now add `'django_extensions',` and `rest_framework,` to INSTALLED_APPS in settings.py
+# also add the below to (the bottom of) settings.py:
+# ```
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
+# ```
+# (Just some basic defaults for django-rest-framework)
 
 touch jobapps/urls.py
 # add an index url
 # add an index view
 # add some models
 
-## setup the DB
+### setup the DB
 psql
 # CREATE DATABASE jobapps OWNER michaelsafdieh;
 # \q
 
 # add the following to DATABASES in settings.py:
+# ```
 # 'default': {
 #     'ENGINE': 'django.db.backends.postgresql',
 #     'NAME': 'jobapps',
@@ -30,6 +40,8 @@ psql
 #     'HOST': '127.0.0.1',
 #     'PORT': '5432',
 # }
+# ```
+
 pip install psycopg2
 ./manage.py makemigrations jobapps
 ./manage.py migrate
@@ -37,3 +49,5 @@ pip install psycopg2
 # go to http://127.0.0.1:8000/jobapps/ in browser to check that initial index page is working properly
 
 pip freeze > ~/career_proj/requirements.txt
+
+
